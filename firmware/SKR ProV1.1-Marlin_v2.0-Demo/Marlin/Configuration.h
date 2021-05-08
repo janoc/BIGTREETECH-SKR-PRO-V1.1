13b7ee750fae3ec977a4deeacec1a87b2a7f8aac
Jan Ciger <jan.ciger@gmail.com>
2021-05-08 23:23:20 +0200

    Machine name

--- firmware/SKR ProV1.1-Marlin_v2.0-Demo/Marlin/Configuration.h
+++ firmware/SKR ProV1.1-Marlin_v2.0-Demo/Marlin/Configuration.h
@@ -137,7 +137,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "Ender-3"
+#define CUSTOM_MACHINE_NAME "Nophead's Mendel90"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
